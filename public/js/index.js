f5388c4c3b62cbe0715bf5c523befac89f23ea01
animsaj <animsaj70@gmail.com>
2018-03-12 18:50:41 +0200

    Greating new user and notifying others

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,10 @@
 var socket = io();
 socket.on("connect", function() {
   console.log("Connected to the server");
-  socket.emit("createMessage", {
-    from: "Danka",
-    text: "Mamaaaa"
+  socket.on("disconnect", function() {
+    console.log("Disconnected from the server");
   });
-});
-socket.on("disconnect", function() {
-  console.log("Disconnected from the server");
-});
-socket.on("newMessage", function(msg) {
-  console.log(msg);
+  socket.on("newMessage", function(msg) {
+    console.log(msg);
+  });
 });
--- server/server.js
+++ server/server.js
@@ -13,14 +13,19 @@ app.use(express.static(path.join(__dirname, "../public")));
 io.on("connection", socket => {
   console.log("New user connected");
   socket.emit("newMessage", {
-    from: "Koula",
-    text: "guf guf",
+    from: "Admin",
+    text: "Welcome to the chat",
     timestamp: new Date(Date.now()).toLocaleString()
   });
-  socket.on("createMessage", newMessage => {
-    newMessage.timestamp = new Date(Date.now()).toLocaleString();
-    socket.emit("newMessage", newMessage);
-  });
+  socket.broadcast.emit("newMessage", {
+    from: "admin",
+    text: "New user has joined the chat",
+    timestamp: new Date(Date.now()).toLocaleString()
+  }),
+    socket.on("createMessage", newMessage => {
+      newMessage.timestamp = new Date(Date.now()).toLocaleString();
+      io.emit("newMessage", newMessage);
+    });
   socket.on("disconnect", () => {
     console.log("User disconnected");
   });
